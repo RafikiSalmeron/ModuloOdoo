7e8ead077bd17608098b7a4da894533542878aaa
RafikiSalmeron <45825538+RafikiSalmeron@users.noreply.github.com>
2019-11-25 19:16:08 +0100

    Delete __manifest__.py

--- ordenador/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'Fabrica',
-    'description': 'Fabrica de ordenadores',
-    'author': 'Rafiki Salmeron',
-    'version': '1.0',
-    'application': True,
-    'data': ['views/ordenador.xml', 'views/placa.xml', 'views/procesador.xml', 'views/fuente.xml', 'views/grafica.xml']
-}
