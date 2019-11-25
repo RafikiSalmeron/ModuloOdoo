4cb5a15e56f98ca16c2d617d378be5f4d7640cdf
RafikiSalmeron <45825538+RafikiSalmeron@users.noreply.github.com>
2019-11-25 19:16:36 +0100

    Add files via upload

---
+++ ordenador/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Fabrica',
+    'description': 'Fabrica de ordenadores',
+    'author': 'Rafiki Salmeron',
+    'version': '1.1',
+    'application': True,
+    'data': ['views/ordenador.xml', 'views/placa.xml', 'views/procesador.xml', 'views/fuente.xml', 'views/prueba.xml', 'views/grafica.xml']
+}
